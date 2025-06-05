89a11c57ae725554a5c3f538f2c42294dd00e5b9
Wahchaka21 <quentyn.petitjean@hotmail.com>
2025-06-05 18:12:26 +0200

    ajout de commentaires

--- cours/src/main.ts
+++ cours/src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 import typescriptLogo from "./typescript.svg";
 import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.ts";
-import "./typage.ts";
+import "./typage.ts"; //l'import c'est pour que littéralement le code marche, si on fait pas d'imprort le code n'est pas lu quand on fait un "dev vite"
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
